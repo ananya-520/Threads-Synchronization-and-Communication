b8796547027c579a533a5843a281c66549e956c2
Ananya <153264820+ananya-520@users.noreply.github.com>
2026-07-01 18:18:00 +0000

    Math Function

---
+++ src/Computation.java
@@ -0,0 +1,17 @@
+/**
+*
+*/
+package project2;
+/**
+*
+*/
+public class Computation {
+	
+	public static int sum(int n) {
+       int total = 0;
+       for(int i = 0; i <= n; i++) {
+           total += i;
+       }
+       return total;
+   }
+}
