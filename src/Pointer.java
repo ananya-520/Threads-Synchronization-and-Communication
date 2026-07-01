db84aa34184b4aa69ddc7dbf6dcaaae4a63f3f98
Ananya <153264820+ananya-520@users.noreply.github.com>
2026-07-01 18:19:42 +0000

    Pointer Creation

---
+++ src/Pointer.java
@@ -0,0 +1,21 @@
+/**
+*
+*/
+package project2;
+/**
+*
+*/
+public class Pointer {
+   // Shared variables
+   public int A1;
+   public int A2;
+   public int A3;
+   public int B1;
+   public int B2;
+   public int B3;
+   // Synchronization flags
+   public boolean A1Done = false;
+   public boolean B2Done = false;
+   public boolean A2Done = false;
+   public boolean B3Done = false;
+}
